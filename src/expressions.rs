b51148756cb107c8f71ea200123d1ad7ea95d283
Bartosz Telenczuk <bartosz.telenczuk@ext.saint-gobain.com>
2024-05-07 19:40:31 +0200

    implement to-sparse

--- src/expressions.rs
+++ src/expressions.rs
@@ -1,7 +1,8 @@
 #![allow(clippy::unused_unit)]
-use polars::prelude::*;
-use pyo3_polars::derive::polars_expr;
 use polars::prelude::arity::binary_elementwise;
+use polars::prelude::*;
+use pyo3::types::PyIterator;
+use pyo3_polars::derive::polars_expr;
 use std::fmt::Write;
 
 #[polars_expr(output_type=String)]
@@ -15,9 +16,7 @@ fn pig_latinnify(inputs: &[Series]) -> PolarsResult<Series> {
     Ok(out.into_series())
 }
 
-
-fn sum_columns(left: Int64Chunked, right: &Series) -> Int64Chunked
-{
+fn sum_columns(left: Int64Chunked, right: &Series) -> Int64Chunked {
     binary_elementwise(
         &left,
         right.i64().unwrap(),
@@ -25,7 +24,7 @@ fn sum_columns(left: Int64Chunked, right: &Series) -> Int64Chunked
             (Some(left), Some(right)) => Some(left + right),
             _ => None,
         },
-   )
+    )
 }
 
 #[polars_expr(output_type=Int64)]
@@ -39,19 +38,60 @@ fn sum_i64(inputs: &[Series]) -> PolarsResult<Series> {
 }
 
 fn list_idx_dtype(input_fields: &[Field]) -> PolarsResult<Field> {
-    let field = Field::new(input_fields[0].name(), DataType::List(Box::new(DataType::Int64)));
+    let field = Field::new(
+        input_fields[0].name(),
+        DataType::List(Box::new(DataType::Int64)),
+    );
     Ok(field.clone())
 }
 
 #[polars_expr(output_type_func=list_idx_dtype)]
 fn to_sparse(inputs: &[Series]) -> PolarsResult<Series> {
     let left = inputs[0].i64()?;
-    // let right: &Int64Chunked = inputs[1].i64()?;
-    // Note: there's a faster way of summing two columns, see
-    // section 7.
-    let mut builder = ListPrimitiveChunkedBuilder::<Int64Type>::new(left.name(), left.len(), inputs.len(), DataType::Int64);
+    let mut cols: Vec<PolarsResult<&ChunkedArray<Int64Type>>> = inputs
+        .iter()
+        .map(|x| x.i64())
+        .collect::<Vec<PolarsResult<&ChunkedArray<Int64Type>>>>();
+    let mut cols: Vec<(&str, Box<dyn PolarsIterator<Item = Option<i64>>>)> = cols
+        .into_iter()
+        .map(|x| {
+            let v = x.unwrap();
+            (v.name(), v.into_iter())
+        })
+        .collect();
 
-    left.for_each(|x| builder.append_series(&Series::new(&"empty", [x, x])).unwrap());
+    let mut builder: ListPrimitiveChunkedBuilder<Int64Type> =
+        ListPrimitiveChunkedBuilder::new(left.name(), left.len(), inputs.len(), DataType::Int64);
+
+    loop {
+        let values: Option<Vec<Option<i64>>> = cols
+            .iter_mut()
+            .map(|(name, x)| {
+                let value = x.next();
+                match value {
+                    Some(Some(v)) => {
+                        if (v == 1) {
+                            Some(Some(name.parse::<i64>().unwrap()))
+                        } else {
+                            Some(None)
+                        }
+                    }
+                    Some(None) => Some(None),
+                    None => None,
+                }
+            })
+            .collect();
+        if let Some(vec) = values {
+            builder.append_series(&Series::from_vec(
+                &"row",
+                vec.iter()
+                    .filter_map(|x: &Option<i64>| *x)
+                    .collect::<Vec<i64>>(),
+            ))?;
+        } else {
+            break;
+        }
+    }
     let out = builder.finish();
     Ok(out.into_series())
 }
